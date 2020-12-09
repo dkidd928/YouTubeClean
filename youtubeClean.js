fce1ed6f89f52abc1cdf8e8cb9c92c55979c71f8
Derek Kidd <dkidd928@users.noreply.github.com>
2020-12-08 21:37:22 -0600

    Update youtubeClean.js
    fixed playlist page usage to properly use "playlist" in url instead of "null" when video parameter not present

--- youtubeClean.js
+++ youtubeClean.js
@@ -5,11 +5,11 @@
  * @summary Minimized for typical use as a bookmarklet in web browsers.
  * 
  * @license The Unlicense, http://unlicense.org/
- * @version 0.2.0
+ * @version 0.2.1
  * @author  Derek Kidd
  * @updated 12.08.2020
  * @link    https://github.com/dkidd928/YouTubeClean
  * 
  */
 
-javascript:!function(){var e=new URLSearchParams(window.location.search),a=e.get("v"),n=e.get("list");n=null!=n?"?list="+n:"",location.href="/embed/"+a+n}();
+javascript:!function(){var l=new URLSearchParams(window.location.search),a=l.get("v"),e=l.get("list");null!=e?(e="?list="+e,null==a&&(a="playlist")):e="",location.href="/embed/"+a+e}();
