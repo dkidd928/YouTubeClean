ce850697bb09fbb21dd97152b5edbb88c7100f95
Derek Kidd <dkidd928@users.noreply.github.com>
2020-12-08 21:02:45 -0600

    Update youtubeClean.js
    Rewritten from ground up. 0.1.0 bugs resolved using new techniques.

--- youtubeClean.js
+++ youtubeClean.js
@@ -5,11 +5,11 @@
  * @summary Minimized for typical use as a bookmarklet in web browsers.
  * 
  * @license The Unlicense, http://unlicense.org/
- * @version 0.1.0
+ * @version 0.2.0
  * @author  Derek Kidd
- * @updated 12.02.2016
+ * @updated 12.08.2020
  * @link    https://github.com/dkidd928/YouTubeClean
  * 
  */
 
-javascript:(function(){u=location.href;if(u.includes("list")){if(u.includes("playlist")){location.href=u.replace('playlist?','embed/playlist?');}else {if(u.includes("index")){u=u.replace(/&index=[0-9]*/,"");}location.href=u.replace('watch?v=','embed/').replace('&list=','?list=');}}else{location.href = u.replace('watch?v=','embed/');}})();
+javascript:(!function(){var e=new URLSearchParams(window.location.search),a=e.get("v"),n=e.get("list");n=null!=n?"?list="+n:"",location.href="/embed/"+a+n}();
