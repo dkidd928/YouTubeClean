57e065db409bb3e90f4165ac605dca01d75a5100
Derek Kidd <dkidd928@users.noreply.github.com>
2020-12-08 20:59:25 -0600

    Update youtubeClean_orig.js
    Rewritten from ground up. 0.1.0 bugs resolved using new techniques.

--- youtubeClean_orig.js
+++ youtubeClean_orig.js
@@ -2,41 +2,26 @@
  * youtubeClean_orig.js
  * YouTubeClean
  * 
- * @summary Original JavaScript program with comments.
- * 
  * @license The Unlicense, http://unlicense.org/
- * @version 0.1.0
+ * @version 0.2.0
  * @author  Derek Kidd
- * @updated 12.02.2016
+ * @updated 12.08.2020
  * @link    https://github.com/dkidd928/YouTubeClean
  * 
  */
 
-function youtubeClean() {
-    //store URI of the current tab
-    uri = location.href;
-    //check if URI is part of a playlist
-    if (uri.includes("list")) {
-        //check if URI is a playlist page
-        if (uri.includes("playlist")) {
-            //script executed on a playlist page
-            //navigate to embedded playlist equivalent
-            location.href = uri.replace('playlist?','embed/playlist?');
-        }
-        else {
-            //script executed on a video page as part of a playlist
-            if (uri.includes("index")) {
-                //current video was not first video in playlist
-                uri = uri.replace(/&index=[0-9]*/,"");
-            }
-            //navigate to embedded playlist equivalent from current video
-            location.href = uri.replace('watch?v=','embed/').replace('&list=','?list=');
-        }
+function ytClean() {
+    var urlParams = new URLSearchParams(window.location.search);
+    var video = urlParams.get('v');
+    var playlist = urlParams.get('list');
+    if (playlist != null) {
+        playlist = "?list=" + playlist;
     }
     else {
-        //navigate to embedded video equivalent
-        location.href = uri.replace('watch?v=','embed/');
+        playlist = "";
     }
+
+    location.href = "/embed/" + video + playlist;
 }
 
-youtubeClean();
+ytClean();
